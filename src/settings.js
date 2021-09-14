2ba3618776400d43ff9e774066bd3bbfe1f51709
titanium_ydtc <853606609@qq.com>
2021-09-14 15:34:24 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    // 项目名称
+    title: 'Vue Admin Template',
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    // 是否固定头部
+    fixedHeader: false,
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    // 是否显示左侧菜单logo
+    sidebarLogo: false
+}
